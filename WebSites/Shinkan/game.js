d7c816dfcd9019730d556218c8b4f17a88991dd9
Kudo <shigure@pankaku.com>
2013-04-11 16:52:53 +0900

    熊のメソッド追加と整理

--- WebSites/Shinkan/game.js
+++ WebSites/Shinkan/game.js
@@ -61,144 +61,159 @@ window.onload = function () {
         map.loadData(blocks);
 
         Bear = enchant.Class.create(Sprite,{
-          initialize:function(x,y){
-            enchant.Sprite.call(this,32,32);
-            this.x = x;
-            this.y = y;
-            this.vx = 0;
-            this.vy = 0;
-            this.ax = 0;
-            this.ay = 0;
-            this.pose = 0;
-            this.jumping = true;
-            this.jumpBoost = 0;
-            this.image = game.assets['images/chara1.gif'];
-          },
-          dying:function(){
+            initialize:function(x,y){
+                enchant.Sprite.call(this,32,32);
+                this.x = x;
+                this.y = y;
+                this.vx = 0;
+                this.vy = 0;
+                this.ax = 0;
+                this.ay = 0;
+                this.friction = 0;
+                this.jumping = true;
+                this.jumpBoost = 0;
+                this.alive = true;
+                this.count = 0;
+                this.image = game.assets['images/chara1.gif'];
+            },
+            setMoveDirection:function(){
+                if (game.input.left){
+                  this.ax = -0.5;
+                  this.scaleX = -1;
+                } else if (game.input.right){
+                    this.ax = 0.5;
+                    this.scaleX = 1;
+                } else {
+                    this.ax = 0;
+                }
+
+                if (this.ax != 0) {
+                    if (game.frame % 3 == 0) {
+                        this.frame %= 2;
+                        this.frame++;
+                    }
+                }
+            },
+            calcFriction:function(){
+                if(this.vx >= 0){
+                  this.friction = this.vx > 0.3 ? -0.3 : -this.vx;
+                } else {
+                  this.friction = this.vx < -0.3 ? 0.3 : -this.vx;
+                }
+            },
+            move:function(){
+                this.vx += this.ax + this.friction;
+                this.vy += this.ay + 2; // 2 is gravity
+                this.vx = Math.min(Math.max(this.vx, -10), 10);
+                this.vy = Math.min(Math.max(this.vy, -10), 10);
+                var dest = new Rectangle(
+                    this.x + this.vx + 5, this.y + this.vy + 2,
+                    this.width - 10, this.height - 2
+                );
+                this.jumping = true;
+                if (dest.x < -stage.x) {
+                    dest.x = -stage.x;
+                    this.vx = 0;
+                }
+                while (true) {
+                    var boundary, crossing;
+                    var dx = dest.x - this.x - 5;
+                    var dy = dest.y - this.y - 2;
+                    if (dx > 0 && Math.floor(dest.right / 16) != Math.floor((dest.right - dx) / 16)) {
+                        boundary = Math.floor(dest.right / 16) * 16;
+                        crossing = (dest.right - boundary) / dx * dy + dest.y;
+                        if ((map.hitTest(boundary, crossing) && !map.hitTest(boundary - 16, crossing)) ||
+                            (map.hitTest(boundary, crossing + dest.height) && !map.hitTest(boundary - 16, crossing + dest.height))) {
+                            this.vx = 0;
+                            dest.x = boundary - dest.width - 0.01;
+                            continue;
+                        }
+                    } else if (dx < 0 && Math.floor(dest.x / 16) != Math.floor((dest.x - dx) / 16)) {
+                        boundary = Math.floor(dest.x / 16) * 16 + 16;
+                        crossing = (boundary - dest.x) / dx * dy + dest.y;
+                        if ((map.hitTest(boundary - 16, crossing) && !map.hitTest(boundary, crossing)) ||
+                            (map.hitTest(boundary - 16, crossing + dest.height) && !map.hitTest(boundary, crossing + dest.height))) {
+                            this.vx = 0;
+                            dest.x = boundary + 0.01;
+                            continue;
+                        }
+                    }
+                    if (dy > 0 && Math.floor(dest.bottom / 16) != Math.floor((dest.bottom - dy) / 16)) {
+                        boundary = Math.floor(dest.bottom / 16) * 16;
+                        crossing = (dest.bottom - boundary) / dy * dx + dest.x;
+                        if ((map.hitTest(crossing, boundary) && !map.hitTest(crossing, boundary - 16)) ||
+                            (map.hitTest(crossing + dest.width, boundary) && !map.hitTest(crossing + dest.width, boundary - 16))) {
+                            this.jumping = false;
+                            this.vy = 0;
+                            dest.y = boundary - dest.height - 0.01;
+                            continue;
+                        }
+                    } else if (dy < 0 && Math.floor(dest.y / 16) != Math.floor((dest.y - dy) / 16)) {
+                        boundary = Math.floor(dest.y / 16) * 16 + 16;
+                        crossing = (boundary - dest.y) / dy * dx + dest.x;
+                        if ((map.hitTest(crossing, boundary - 16) && !map.hitTest(crossing, boundary)) ||
+                            (map.hitTest(crossing + dest.width, boundary - 16) && !map.hitTest(crossing + dest.width, boundary))) {
+                            this.vy = 0;
+                            dest.y = boundary + 0.01;
+                            continue;
+                        }
+                    }
+
+                    break;
+                }
+                this.x = dest.x - 5;
+                this.y = dest.y - 2;
+            },
+            jump:function(){
+                if (this.jumping) {
+                    if (!game.input.up || --this.jumpBoost < 0) {
+                        this.ay = 0;
+                    }
+                } else {
+                    if (game.input.up) {
+                        this.jumpBoost = 5;
+                        this.ay = -5;
+                        game.assets['sounds/jump.wav'].clone().play();
+                    }
+                }
+            },
+            dead:function(){
                 game.assets['sounds/gameover.wav'].play();
                 var score = Math.round(bear.x);
                 this.frame = 3;
-                this.vy = -40;
-                this.dead();
-          },
-          dead:function(){
+                this.vy = -10;
+                if(++this.count > 5){
+                    this.dying();
+                }
+            },
+            dying:function(){
                 this.addEventListener('enterframe', function () {
-                    this.vy += 1;
+                    this.vy += 2;
                     this.y += Math.min(Math.max(this.vy, -10), 10);
                     if (this.y > 320) {
-                    game.end(score, score + 'mで死にました');
-                }
-            });
-            this.removeEventListener('enterframe', arguments.callee);
-          }
+                        game.end(score, score + 'mで死にました');
+                    }
+                });
+                this.removeEventListener('enterframe', arguments.callee);
+            }
         });
         var bear = new Bear(8, -32);
-        
+
         bear.addEventListener('enterframe', function (e) {
-            var friction = 0;
-            if(this.vx >= 0){
-              friction = this.vx > 0.3 ? -0.3 : -this.vx;
+            if(this.alive){
+                this.setMoveDirection();
+                this.calcFriction();
+                this.move();
+                this.jump();
             } else {
-              friction = this.vx < -0.3 ? 0.3 : -this.vx;
+                this.dead();
             }
-
-            if (this.jumping) {
-                if (!game.input.up || --this.jumpBoost < 0) {
-                    this.ay = 0;
-                }
-            } else {
-                if (game.input.up) {
-                    this.jumpBoost = 5;
-                    this.ay = -5;
-                    game.assets['sounds/jump.wav'].clone().play();
-                }
-            }
-
-            this.ax = 0;
-            if (game.input.left){
-              this.ax -= 0.5;
-              this.scaleX = -1;
-            }
-            if (game.input.right){
-                this.ax += 0.5;
-                this.scaleX = 1;
-            }
-
-            if (this.ax != 0) {
-                if (game.frame % 3 == 0) {
-                    this.frame %= 2;
-                    this.frame++;
-                }
-            }
-            this.vx += this.ax + friction;
-            this.vy += this.ay + 2; // 2 is gravity
-            this.vx = Math.min(Math.max(this.vx, -10), 10);
-            this.vy = Math.min(Math.max(this.vy, -10), 10);
-            var dest = new Rectangle(
-                this.x + this.vx + 5, this.y + this.vy + 2,
-                this.width - 10, this.height - 2
-            );
-            this.jumping = true;
-            if (dest.x < -stage.x) {
-                dest.x = -stage.x;
-                this.vx = 0;
-            }
-            while (true) {
-                var boundary, crossing;
-                var dx = dest.x - this.x - 5;
-                var dy = dest.y - this.y - 2;
-                if (dx > 0 && Math.floor(dest.right / 16) != Math.floor((dest.right - dx) / 16)) {
-                    boundary = Math.floor(dest.right / 16) * 16;
-                    crossing = (dest.right - boundary) / dx * dy + dest.y;
-                    if ((map.hitTest(boundary, crossing) && !map.hitTest(boundary - 16, crossing)) ||
-                        (map.hitTest(boundary, crossing + dest.height) && !map.hitTest(boundary - 16, crossing + dest.height))) {
-                        this.vx = 0;
-                        dest.x = boundary - dest.width - 0.01;
-                        continue;
-                    }
-                } else if (dx < 0 && Math.floor(dest.x / 16) != Math.floor((dest.x - dx) / 16)) {
-                    boundary = Math.floor(dest.x / 16) * 16 + 16;
-                    crossing = (boundary - dest.x) / dx * dy + dest.y;
-                    if ((map.hitTest(boundary - 16, crossing) && !map.hitTest(boundary, crossing)) ||
-                        (map.hitTest(boundary - 16, crossing + dest.height) && !map.hitTest(boundary, crossing + dest.height))) {
-                        this.vx = 0;
-                        dest.x = boundary + 0.01;
-                        continue;
-                    }
-                }
-                if (dy > 0 && Math.floor(dest.bottom / 16) != Math.floor((dest.bottom - dy) / 16)) {
-                    boundary = Math.floor(dest.bottom / 16) * 16;
-                    crossing = (dest.bottom - boundary) / dy * dx + dest.x;
-                    if ((map.hitTest(crossing, boundary) && !map.hitTest(crossing, boundary - 16)) ||
-                        (map.hitTest(crossing + dest.width, boundary) && !map.hitTest(crossing + dest.width, boundary - 16))) {
-                        this.jumping = false;
-                        this.vy = 0;
-                        dest.y = boundary - dest.height - 0.01;
-                        continue;
-                    }
-                } else if (dy < 0 && Math.floor(dest.y / 16) != Math.floor((dest.y - dy) / 16)) {
-                    boundary = Math.floor(dest.y / 16) * 16 + 16;
-                    crossing = (boundary - dest.y) / dy * dx + dest.x;
-                    if ((map.hitTest(crossing, boundary - 16) && !map.hitTest(crossing, boundary)) ||
-                        (map.hitTest(crossing + dest.width, boundary - 16) && !map.hitTest(crossing + dest.width, boundary))) {
-                        this.vy = 0;
-                        dest.y = boundary + 0.01;
-                        continue;
-                    }
-                }
-
-                break;
-            }
-            this.x = dest.x - 5;
-            this.y = dest.y - 2;
 
             if (this.y > 320) {
-                this.dying();
+                this.alive = false;
             }
         });
 
-       
-       
         var items = new Array(10);
         for (var i = 0; i < items.length ; i++) {
             items[i] = new Item();
